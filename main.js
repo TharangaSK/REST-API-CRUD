3577cd8f25e13302ea9fedd2e3951d100423464c
Tharanga Sampath Keerthirathne <117074902+TharangaSK@users.noreply.github.com>
2024-04-05 22:34:50 +0530

    Initial commit
    Added all files to the repository

---
+++ main.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+//temporary testing data array 
+const students = [
+    { id: 1, name: "stu.01" },
+    { id: 2, name: "stu.02" },
+    { id: 3, name: "stu.03" }
+];
+
+//READ
+app.get("/", (req, res) => {
+    res.send("Hello my name is Tharanga Sampath Keerthirathne");
+});
+
+app.get("/students", (req, res) => {
+    res.json(students);
+});
+//READ by specific ID
+app.get("/students/:id", (req, res) => {
+    //look up at the student to check if it exists
+    const student = students.find(c => c.id === parseInt(req.params.id))
+    if (!student) return res.status(404).send('Not Found');
+    res.send(student);
+});
+
+//CREATE
+app.post("/create/students", (req, res) => {
+    const student = {
+        id: students.length + 1,
+        name: req.body.name
+    };
+    students.push(student);
+    res.send(student);
+});
+
+//UPDATE
+app.put('/update/:id', (req, res) => {
+    //look up at the student to check if it exists
+    const student = students.find(c => c.id === parseInt(req.params.id))
+    if (!student) return res.status(404).send('Not Found');
+
+    //update the student
+    student.name = req.body.name;
+    res.send(student);
+});
+
+//DELETE
+app.delete('/delete/:id', (req, res) => {
+    //look up at the student to check if it exists
+    const student = students.find(c => c.id === parseInt(req.params.id))
+    if (!student) return res.status(404).send('Not Found');
+
+    //delete the student
+    const index = students.indexOf(student);
+    students.splice(index, 1);
+    res.send(student)
+});
+
+
+app.listen(port, () => {
+    console.log(`example app listening on ${port}`);
+});
